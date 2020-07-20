b730c13cb478e8baee5d57f8c9d5e5db67bdb877
Mário César Silva <mcgato33@hotmail.com>
2020-07-20 12:23:06 -0100

    styled-components e estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import Globalstyle from './styles/global';
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <Globalstyle />
+  </>
 );
 
 export default App;
