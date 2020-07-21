2001d826093ba012546c8b425bfd48e0b2346c11
Mário César Silva <mcgato33@hotmail.com>
2020-07-21 00:03:02 -0100

    criando API
    primeiro passos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository() {
+
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github.</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form>
+        <input
+          value={newRepo} 
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório" 
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
@@ -28,32 +40,7 @@ const Dashboard: React.FC = () => {
 
           <FiChevronRight size={20}/>
         </a>
-
-        <a href="test">
-          <img 
-            src="https://avatars3.githubusercontent.com/u/24758286?s=460&u=d3b6fcc00916078ae870fd88b802b006b7c3ac72&v=4" 
-            alt="Mário César"
-          />
-          <div>
-            <strong>mariocesar33/the-box</strong>
-            <p>design responsivo e assimétrico HTML e CSS</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
-
-        <a href="test">
-          <img 
-            src="https://avatars3.githubusercontent.com/u/24758286?s=460&u=d3b6fcc00916078ae870fd88b802b006b7c3ac72&v=4" 
-            alt="Mário César"
-          />
-          <div>
-            <strong>mariocesar33/the-box</strong>
-            <p>design responsivo e assimétrico HTML e CSS</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
+ 
       </Repositories>
     </>
   );
